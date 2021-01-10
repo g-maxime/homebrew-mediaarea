669ab15071c9ae09cf28a5371f344eb93eba9fe2
MediaArea CI <info@mediaarea.net>
2021-01-10 15:53:53 +0000

    rawcooked 21.01

--- rawcooked.rb
+++ rawcooked.rb
@@ -2,7 +2,7 @@ class Rawcooked < Formula
   desc "Lossless RAW audio-visual data to MKV encoder and decoder"
   homepage "https://mediaarea.net/RAWcooked"
   url "https://mediaarea.net/download/binary/rawcooked/21.01/RAWcooked_CLI_21.01_GNU_FromSource.tar.xz"
-  sha256 "18ab216331526e9ac33bbf1e6b4cc3beec2822ed31f8bc80a48e0a599c7ce1dc"
+  sha256 "39c0f9e5cb0be20340cc3f8be864ddd1e36d66e87b3dabfbbfe45657496f7bc6"
 
   def install
     cd "Project/GNU/CLI" do
