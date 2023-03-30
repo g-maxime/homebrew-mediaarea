bcb78ad2435fe8838ceec1a336b25e2378217349
Maxime Gervais <gervais.maxime@gmail.com>
2023-03-30 11:41:35 +0200

    MediaConch cask v23.03
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Casks/mediaconch-gui.rb
+++ Casks/mediaconch-gui.rb
@@ -1,6 +1,6 @@
 cask 'mediaconch-gui' do
-  version '18.03.2'
-  sha256 '2dc51e39b74d516e094063860015c7ff37d6a038009e69dd121b7ff905911bbc'
+  version '23.03'
+  sha256 'https://mediaarea.net/download/binary/mediaconch-gui/#{version}/MediaConch_GUI_#{version}_Mac.dmg'
 
   url "https://mediaarea.net/download/binary/mediaconch-gui/#{version}/MediaConch_GUI_#{version}_Mac.dmg"
   appcast 'https://github.com/MediaArea/MediaConch_SourceCode/releases.atom'
