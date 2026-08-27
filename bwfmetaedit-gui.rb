8b2b4fb4a025564a20131fb1bc273cbb1b36b685
Maxime Gervais <gervais.maxime@gmail.com>
2026-08-27 15:03:24 +0200

    bwfmetaedit-gui: Update to version 26.08
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- bwfmetaedit-gui.rb
+++ bwfmetaedit-gui.rb
@@ -1,15 +1,28 @@
 class BwfmetaeditGui < Formula
   desc "Embed, validate and export Broadcast WAVE Format (BWF) metadata"
   homepage "https://mediaarea.net/BWFMetaEdit"
-  url "https://mediaarea.net/download/binary/bwfmetaedit-gui/26.01/BWFMetaEdit_GUI_26.01_GNU_FromSource.tar.xz"
-  sha256 "b670b196397d7d94c83b97b50f4704a66e01de113f1f68e7922e3d45e4fab4d8"
+  url "https://mediaarea.net/download/binary/bwfmetaedit-gui/26.08/BWFMetaEdit_GUI_26.08_GNU_FromSource.tar.xz"
+  sha256 "8960ee20329c7773f0979af42dbd7d2edc4dafa3584bc21815e998498d0b89fa"
 
   depends_on "qt"
+  depends_on "rust" => :build
 
   def install
+    dylib = buildpath/"Source/ThirdParty/c2pa-rs/target/release/libc2pa_c.dylib"
+    cd "Source/ThirdParty/c2pa-rs" do
+      system "cargo", "build", "--release", "--locked", "-p", "c2pa-c-ffi",
+             "--no-default-features", "--features", "rust_native_crypto, http, file_io"
+
+      MachO::Tools.change_dylib_id(dylib, "@rpath/libc2pa_c.dylib")
+    end
+
     cd "Project/QtCreator" do
-      system "#{Formula["qt"].bin}/qmake"
+      system "#{Formula["qt"].bin}/qmake", "ENABLE_C2PA=dynamic"
       system "make"
+
+      frameworks = Pathname.pwd/"BWF MetaEdit.app/Contents/Frameworks"
+      frameworks.mkpath
+      cp dylib, frameworks/"libc2pa_c.dylib"
 
       prefix.install "BWF MetaEdit.app"
 
@@ -20,5 +33,6 @@ class BwfmetaeditGui < Formula
 
   test do
     assert_path_exists bin/"bwfmetaedit-gui"
+    assert_path_exists prefix/"BWF MetaEdit.app/Contents/Frameworks/libc2pa_c.dylib"
   end
 end
